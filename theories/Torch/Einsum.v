32b5e6ca458f473fdda0dd08efc589a0c6807a1c
Jason Gross <jgross@mit.edu>
2023-07-14 15:09:49 -0700

    WIP on einsum that doesn't rely on names in Ltac2

--- theories/Torch/Einsum.v
+++ theories/Torch/Einsum.v
@@ -9,17 +9,6 @@ Import Ltac2.
 Import FixNotationsForPerformance MakeAbbreviations Printf.
 
 Module Import Internals.
-  Ltac2 rec decompose_lam_idents (c : constr) : ident list
-    := match Constr.Unsafe.kind_nocast c with
-       | Constr.Unsafe.Lambda b body
-         => let rest := decompose_lam_idents body in
-            match Constr.Binder.name b with
-            | Some n => n :: rest
-            | None => rest
-            end
-       | _ => []
-       end.
-
   Ltac2 rec get_body (at_head : bool) (c : constr) :=
     match Constr.Unsafe.kind_nocast c with
     | Constr.Unsafe.Var v
@@ -36,29 +25,78 @@ Module Import Internals.
     | _ => c
     end.
 
-  Ltac2 rec make_einsum_noclose_nolift (sum_to : constr (* max *) -> constr (* body *) -> constr) (intT : constr) (ids : ident list) (body : constr) : constr
-    := match ids with
-       | [] => body
-       | id :: ids
-         => let body := make_einsum_noclose_nolift sum_to intT ids body in
-            let body := mkLambda (Constr.Binder.make (Some id) intT) body in
-            sum_to (Constr.Unsafe.make (Constr.Unsafe.Var id)) body
+  Ltac2 shape_to_list (c : constr) : constr list
+    := let rec aux c acc
+         := lazy_match! c with
+            | Shape.nil => acc
+            | Shape.snoc ?cs ?c
+              => aux cs (c :: acc)
+            end in
+       aux c [].
+
+  Ltac2 ident_of_constr (c : constr) : ident option
+    := match Constr.Unsafe.kind_nocast c with
+       | Constr.Unsafe.Var v => Some v
+       | _ => None
        end.
 
+  Ltac2 toplevel_rels (c : constr) : int list
+    := let rec aux (acc : int list) (c : constr)
+         := match Constr.Unsafe.kind_nocast c with
+            | Constr.Unsafe.Rel i => i :: acc
+            | Constr.Unsafe.App f args
+              => let acc := aux acc f in
+                 Array.fold_right aux acc args
+            | _ => acc
+            end in
+       List.sort_uniq Int.compare (aux [] c).
+
+  Ltac2 rec insert_einsums_helper (rawindexT : constr) (sum_to : (* stop : *) constr -> (* body : *) constr -> constr) (names : ident option list) (c : constr) : (* used_rels : *) int list * (* cur_rel : *) int * (* new_body : *) constr
+    := match Constr.Unsafe.kind_nocast c with
+       | Constr.Unsafe.Lambda b body
+         => let (name, names) := match names with
+                                 | name :: names => (name, names)
+                                 | [] => (Constr.Binder.name b, names)
+                                 end in
+            let (used_rels, cur_rel, body) := insert_einsums_helper rawindexT sum_to names body in
+            let (cur_rel_used, used_rels)
+              := match used_rels with
+                 | r :: rs
+                   => if Int.equal cur_rel r
+                      then (true, rs)
+                      else (false, used_rels)
+                 | [] => (false, used_rels)
+                 end in
+            let b_as_rawindex := Constr.Binder.make name rawindexT in
+            (used_rels, Int.add cur_rel 1,
+              if cur_rel_used
+              then mkLambda b (sum_to (mkRel 1) (Constr.Unsafe.liftn 1 1 (mkLambda b_as_rawindex body)))
+              else mkLambda b body)
+       | _ => (toplevel_rels c, 1, c)
+       end.
+
+  Ltac2 Type exn ::= [ InternalEinsumBadKind (Constr.Unsafe.kind) ].
+
   Local Notation try_tc := (ltac:(try typeclasses eauto)) (only parsing).
-  Ltac2 make_einsum (src_ids : ident list list) (dest_ids : ident list) (body : constr) : constr
+  Ltac2 make_einsum (shapes : constr list) (body : constr) : constr
     := let ty := Constr.type body in
-       let src_ids := List.flat_map (fun x => x) src_ids in
-       let einsum_ids := List.uniq Ident.equal (List.diff Ident.equal src_ids dest_ids) in
-       let einsum_ids_rev := List.rev einsum_ids in
-       let nbinders := List.length einsum_ids in
-       let body := Constr.Unsafe.liftn nbinders 1 body in
-       let body := Constr.Unsafe.closenl einsum_ids_rev 1 body in
+       let c := Std.eval_pattern (List.map (fun s => (s, Std.AllOccurrences)) shapes) body in
+       let rawindexT := 'RawIndexType in
        let start := '(0%uint63) in
        let step := '(1%uint63) in
        let sum := '(@Reduction.sum $ty try_tc try_tc) in
        let sum_to stop body := mkApp sum [start; stop; step; body] in
-       make_einsum_noclose_nolift sum_to 'int einsum_ids body.
+       let names := List.map ident_of_constr shapes in
+       match Constr.Unsafe.kind_nocast c with
+       | Constr.Unsafe.App f shape_args
+         => let (_, _, f) := insert_einsums_helper rawindexT sum_to names f in
+            let f := Constr.Unsafe.make (Constr.Unsafe.App f shape_args) in
+            let f := (eval cbv beta in f) in
+            f
+       | k
+         => Control.throw (InternalEinsumBadKind k)
+       end.
+
 
   Ltac subst_type_lets_in_goal _ :=
     repeat match goal with
@@ -69,13 +107,12 @@ End Internals.
 Local Notation try_tc := (ltac2:(ltac1:(try typeclasses eauto))) (only parsing).
 
 (* Kludge around COQBUG(https://github.com/coq/coq/issues/17833#issuecomment-1627483008) *)
-Local Notation indirect_einsum tensor_value kidxs iidxs jidxs
+Local Notation indirect_einsum tensor_value ishape jshape
   := ltac2:(let get_body v := get_body false (Constr.pretype v) in
-            let get_idents v := decompose_lam_idents (get_body v) in
+            let get_shape v := shape_to_list (get_body v) in
             let t := get_body tensor_value in
-            let src_ids := List.map get_idents [iidxs; jidxs] in
-            let dest_ids := get_idents kidxs in
-            let t := make_einsum src_ids dest_ids t in
+            let shapes := List.append (get_shape ishape) (get_shape jshape) in
+            let t := make_einsum shapes t in
             exact $t)
              (only parsing).
 
@@ -89,42 +126,45 @@ Import RawIndex.UncurryNotation.
 (* TODO: fix naming *)
 #[export] Hint Extern 1 => progress subst_type_lets_in_goal () : typeclass_instances.
 Declare Custom Entry einsum_args.
-TODO: use Ltac/Ltac2 just to strip off the unused einsums, and to push the einsums under the Tensor.uncurry.  Use Gallina to introduce einsums for all shapes.
+(*TODO: use Ltac/Ltac2 just to strip off the unused einsums, and to push the einsums under the Tensor.uncurry.  Use Gallina to introduce einsums for all shapes.*)
 Notation "{{{ {{ i1 .. i_ , j1 .. j_ -> k1 .. k_ }} , t1 , t2 }}}"
   := (match t1%tensor, t2%tensor, _ as A, _ as B, _ as C, _ as r1, _ as r2, _ as r3, _ as s1, _ as s2, _ as s3 return @tensor _ C s3 with
       | t1', t2', A, B, C, r1, r2, r3, s1, s2, s3
         => match t1' : @tensor r1 A s1, t2' : @tensor r2 B s2 return @tensor r3 C s3 with
            | t1', t2'
-             => match make_idxs_for_einsum i1 .. i_,
-                  make_idxs_for_einsum j1 .. j_,
-                  make_idxs_for_einsum k1 .. k_,
-                  (* for typing *)
-                  unify_rank_from_idxs r1 @ i1 .. i_,
-                  unify_rank_from_idxs r2 @ j1 .. j_,
-                  unify_rank_from_idxs r3 @ k1 .. k_
-                    return @tensor r3 C s3
-                with
-                | __EINSUM_IIDXS, __EINSUM_JIDXS, __EINSUM_KIDXS
-                  , _, _, _
-                  => @with_shape
-                       r1 (tensor C s3) s1
-                       (λ i1 .. i_ ,
-                         @with_shape
-                           r2 (tensor C s3) s2
-                           (λ j1 .. j_ ,
-                             (@Tensor.uncurry
-                                r3 C s3
-                                (λ k1 .. k_ ,
-                                  match @Arith.Classes.mul
-                                          A B C try_tc
-                                          (t1' (RawIndex.snoc .. (RawIndex.snoc RawIndex.nil i1) .. i_))
-                                          (t2' (RawIndex.snoc .. (RawIndex.snoc RawIndex.nil j1) .. j_))
-                                        return C
-                                  with
-                                  | __EINSUM_TENSOR_VALUE
-                                    => indirect_einsum
-                                         __EINSUM_TENSOR_VALUE __EINSUM_KIDXS __EINSUM_IIDXS __EINSUM_JIDXS
-                                  end))))
+             => match (* for typing *)
+                 unify_rank_from_idxs r1 @ i1 .. i_,
+                 unify_rank_from_idxs r2 @ j1 .. j_,
+                 unify_rank_from_idxs r3 @ k1 .. k_
+                   return @tensor r3 C s3
+                 with
+               | _, _, _
+                 => @with_shape
+                      r1 (tensor C s3) s1
+                      (λ i1 .. i_ ,
+                        @with_shape
+                          r2 (tensor C s3) s2
+                          (λ j1 .. j_ ,
+                            (match
+                                (Shape.snoc .. (Shape.snoc Shape.nil i1) .. i_),
+                                (Shape.snoc .. (Shape.snoc Shape.nil j1) .. j_)
+                                return @tensor r3 C s3
+                              with
+                              | __I_SHAPE, __J_SHAPE
+                                => @Tensor.uncurry
+                                     r3 C s3
+                                     (λ k1 .. k_ ,
+                                       match @Arith.Classes.mul
+                                               A B C try_tc
+                                               (t1' (RawIndex.snoc .. (RawIndex.snoc RawIndex.nil i1) .. i_))
+                                               (t2' (RawIndex.snoc .. (RawIndex.snoc RawIndex.nil j1) .. j_))
+                                             return C
+                                       with
+                                       | __EINSUM_TENSOR_VALUE
+                                         => indirect_einsum
+                                              __EINSUM_TENSOR_VALUE __I_SHAPE __J_SHAPE
+                                        end)
+                               end)))
                 end
            end
       end)
