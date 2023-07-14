cf4ec36fa16029e67fa8d6e8b24b95c5b2576f5d
Jason Gross <jgross@mit.edu>
2023-07-14 16:57:20 -0700

    More einsum

--- theories/Torch/Einsum.v
+++ theories/Torch/Einsum.v
@@ -50,47 +50,114 @@ Module Import Internals.
             | _ => acc
             end in
        List.sort_uniq Int.compare (aux [] c).
-FIXME REWORK
-  Ltac2 rec insert_einsums_helper (rawindexT : constr) (sum_to : (* stop : *) constr -> (* body : *) constr -> constr) (shapes : constr list) (lift_rel_by : int) (c : constr) : (* used_rels : *) int list * (* cur_rel : *) int * (* new_body : *) constr
-    := match Constr.Unsafe.kind_nocast c with
-       | Constr.Unsafe.Lambda b body
-         => let (name, shape, shapes)
-              := match shapes with
-                 | shape :: shapes => (ident_of_constr shape, shapes, names)
-                 | [] => (Constr.Binder.name b, names)
-                 end in
-            let (used_rels, cur_rel, body) := insert_einsums_helper rawindexT sum_to names body in
-            let (cur_rel_used, used_rels)
-              := match used_rels with
-                 | r :: rs
-                   => if Int.equal cur_rel r
-                      then (true, rs)
-                      else (false, used_rels)
-                 | [] => (false, used_rels)
-                 end in
-            let b := Constr.Binder.make name rawindexT in
-            (used_rels, Int.add cur_rel 1,
-              if cur_rel_used
-              then mkLambda b (sum_to (Constr.Unsafe.liftn lift_rel_by 1 shape) (Constr.Unsafe.liftn 1 1 (mkLambda b body)))
-              else mkLambda b body)
-       | _ => (toplevel_rels c, 1, c)
-       end.
-
-  Ltac2 Type exn ::= [ InternalEinsumBadKind (Constr.Unsafe.kind) ].
 
   Local Notation try_tc := (ltac:(try typeclasses eauto)) (only parsing).
-  Ltac2 make_einsum (shapes : constr list) (body : constr) : constr
-    := let ty := Constr.type body in
-       let c := Std.eval_pattern (List.map (fun s => (s, Std.AllOccurrences)) shapes) body in
-       let rawindexT := 'RawIndexType in
-       let start := '(0%uint63) in
+  (* inserts einsum for all binders *)
+  Ltac2 insert_all_einsums (sum_to : constr -> constr -> constr) (names : ident option list) (body : constr) : constr
+    := let rawindexT := 'RawIndexType in
+       let nbinders := List.length names in
+       let body := Constr.Unsafe.liftn nbinders (Int.add 1 nbinders) body in
+       let rec aux (names : ident option list) (rel_above : int) (body : constr) : (* cur_rel : *) int * constr
+         := match names with
+            | [] => (1, body)
+            | name :: names
+              => let (cur_rel, body) := aux names (Int.add 1 rel_above) body in
+                 (Int.add 1 cur_rel,
+                   sum_to (mkRel (Int.add cur_rel rel_above)) (mkLambda (Constr.Binder.make name rawindexT) body))
+            end in
+       let (_, body) := aux names 0 body in
+       body.
+
+  Ltac2 Type exn ::= [ InternalEinsumNotEqual (constr, constr) ].
+  Ltac2 Type exn ::= [ InternalEinsumNotEnoughArgs (int, constr, constr array) ].
+  Ltac2 Type exn ::= [ InternalEinsumBadKind (Constr.Unsafe.kind) ].
+  (* removes unused einsum *)
+  Ltac2 rec remove_dead_einsum_helper (hd_c : constr) (nargs : int) (names : 'a list) (body : constr) : (* cur_rel : *) int * (* used_rels : *) int list * ((* accumulated shift : *) int * (* body : *) constr)
+    := match names with
+       | [] => (1, toplevel_rels body, (0, body))
+       | _ :: names
+         => match Constr.Unsafe.kind_nocast body with
+            | Constr.Unsafe.App f args
+              => (if Int.ge nargs (Array.length args)
+                  then Control.throw (InternalEinsumNotEnoughArgs nargs f args)
+                  else ());
+                 (let first_args := List.firstn nargs (Array.to_list args) in
+                  let fargs := mkApp f first_args in
+                  if Bool.neg (Constr.equal fargs hd_c)
+                  then Control.throw (InternalEinsumNotEqual fargs hd_c)
+                  else ());
+                 let lam_body_pos := Int.sub (Array.length args) 1 in
+                 let lam_body := Array.get args lam_body_pos in
+                 match Constr.Unsafe.kind_nocast lam_body with
+                 | Constr.Unsafe.Lambda b body
+                   => let (cur_rel, used_rels, (accumulated_shift, body)) := remove_dead_einsum_helper hd_c nargs names body in
+                      let (cur_rel_used, used_rels)
+                        := match used_rels with
+                           | r :: rs => if Int.equal cur_rel r
+                                        then (true, rs)
+                                        else (false, used_rels)
+                           | [] => (false, used_rels)
+                           end in
+                      (Int.add cur_rel 1,
+                        used_rels,
+                        if cur_rel_used
+                        then
+                          let lam_body := mkLambda b body in
+                          (* drop down rels that we dropped *)
+                          let lam_body := Constr.Unsafe.liftn accumulated_shift 1 lam_body in
+                          Array.set args lam_body_pos lam_body;
+                          (0, Constr.Unsafe.make (Constr.Unsafe.App f args))
+                        else
+                          (Int.sub accumulated_shift 1,
+                            body))
+                 | k => Control.throw (InternalEinsumBadKind k)
+                 end
+            | k => Control.throw (InternalEinsumBadKind k)
+            end
+       end.
+
+  Ltac2 remove_dead_einsum (hd_c : constr) (nargs : int) (names : 'a list) (body : constr) : constr
+    := let (_cur_rel, _used_rels, (accumulated_shift, body)) := remove_dead_einsum_helper hd_c nargs names body in
+       (* drop down rels that we dropped *)
+       Constr.Unsafe.liftn accumulated_shift 1 body.
+
+  (* inserts einsum for used binders *)
+  Ltac2 insert_einsums (ty : constr) (names : ident option list) (body : constr) : constr
+    := let start := '(0%uint63) in
        let step := '(1%uint63) in
        let sum := '(@Reduction.sum $ty try_tc try_tc) in
        let sum_to stop body := mkApp sum [start; stop; step; body] in
+       let body := insert_all_einsums sum_to names body in
+       let n_sum_args := match Constr.Unsafe.kind_nocast sum with
+                         | Constr.Unsafe.App _ args => Array.length args
+                         | k => Control.throw (InternalEinsumBadKind k)
+                         end in
+       let body := remove_dead_einsum sum n_sum_args names body in
+       body.
+
+  Ltac2 Type exn ::= [ EinsumExtraNames (Constr.Unsafe.kind, ident option list) ].
+
+  (* inserts Reduction.sum for all binders *)
+  Ltac2 insert_all_einsums_below (ty : constr) (names : ident option list) (body : constr) : constr
+    := let rec aux (cur_names : ident option list) (body : constr)
+         := match cur_names with
+            | [] => insert_einsums ty names body
+            | n :: ns
+              => match Constr.Unsafe.kind_nocast body with
+                 | Constr.Unsafe.Lambda b body
+                   => mkLambda b (aux ns body)
+                 | k => Control.throw (EinsumExtraNames k cur_names)
+                 end
+            end in
+       aux names body.
+
+  Ltac2 make_einsum (shapes : constr list) (body : constr) : constr
+    := let ty := Constr.type body in
+       let c := Std.eval_pattern (List.map (fun s => (s, Std.AllOccurrences)) shapes) body in
        let names := List.map ident_of_constr shapes in
        match Constr.Unsafe.kind_nocast c with
        | Constr.Unsafe.App f shape_args
-         => let (_, _, f) := insert_einsums_helper rawindexT sum_to names f in
+         => let f := insert_all_einsums_below ty names f in
             let f := Constr.Unsafe.make (Constr.Unsafe.App f shape_args) in
             printf "before: %t" f;
             let f := (eval cbv beta in f) in
@@ -183,7 +250,7 @@ Notation "'weaksauce_einsum' x"
                     exact $z)
       end)
        (x custom einsum_args at level 10, at level 10, only parsing).
-(*
+
 Set Printing Implicit.
 Check (weaksauce_einsum {{{ {{ query_pos head_index d_head,
                    key_pos head_index d_head
