b5c1859fa32b8337a9fb0ca20b6ecad8cc609bdb
Quinn Dougherty <quinn@quinnd.net>
2023-10-20 13:39:40 -0700

    direct to upstream nixpkgs unstable since `vcfloat` was merged into it

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Work in progress formalization of mechanistic interpretability arguments on tiny neural nets";
 
   inputs = {
-    nixpkgs-upstream.url = "nixpkgs/nixos-23.05";
-    nixpkgs.url = "github:quinn-dougherty/nixpkgs/init-vcfloat";
+    nixpkgs-stable.url = "nixpkgs/nixos-23.05";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nix-doom-emacs = {
       url = "github:nix-community/nix-doom-emacs";
@@ -12,7 +12,7 @@
   };
 
   outputs =
-    { self, nixpkgs-upstream, nixpkgs, flake-parts, nix-doom-emacs }@inputs:
+    { self, nixpkgs-stable, nixpkgs, flake-parts, nix-doom-emacs }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         ./nix
